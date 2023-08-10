a0ce0b20c2625d141f9b869be72191a64c4dd2b5
Codi McKee <cmckee@tamu.edu>
2023-08-10 15:15:51 -0500

    Update install-intel-hpc-windows.bat

--- install-intel-hpc-windows.bat
+++ install-intel-hpc-windows.bat
@@ -3,10 +3,10 @@
 :: download and unpack installer - HPC KIT
 echo Download oneAPI HPC
 curl.exe --output %TEMP%\webimage.exe --url %1 --retry 5 --retry-delay 5  --log %TEMP%\extract.log
-echo Extract oneAPI Basekit Installer
-start /b /wait %TEMP%\webimage.exe -s -x -f %TEMP%\webimage_extracted 
+echo Extract oneAPI HOC Installer
+start /b /wait %TEMP%\webimage.exe -s -x -f %TEMP%\webimage_extracted_hpc
 del %TEMP%\webimage.exe
 
 :: run installer 
 echo Run oneAPI HPC Installer
-%TEMP%\webimage_extracted\bootstrapper.exe -s --action install --components=intel.oneapi.win.ifort-compiler:intel.oneapi.win.mpi.devel:intel.oneapi.win.cpp-compiler --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0  --log-dir=%TEMP%
+%TEMP%\webimage_extracted_hpc\bootstrapper.exe -s --action install --components=intel.oneapi.win.ifort-compiler:intel.oneapi.win.mpi.devel:intel.oneapi.win.cpp-compiler --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0  --log-dir=%TEMP%
