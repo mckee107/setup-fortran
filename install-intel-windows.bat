695ccbcb962f19973ee4d9992fb1ead1d38cb696
Codi McKee <cmckee@tamu.edu>
2023-08-10 12:14:48 -0500

    Update install-intel-windows.bat

--- install-intel-windows.bat
+++ install-intel-windows.bat
@@ -1,17 +1,17 @@
 @echo off
 
 :: download and unpack installer - BASEKIT
-curl.exe --output %TEMP%\webimage.exe --url %1 --retry 5 --retry-delay 5
-start /b /wait %TEMP%\webimage.exe  -x -f %TEMP%\webimage_extracted 
+curl.exe --output %TEMP%\webimage.exe --url %1 --retry 5 --retry-delay 5  --log %TEMP%\extract.log
+start /b /wait %TEMP%\webimage.exe -s -x -f %TEMP%\webimage_extracted 
 del %TEMP%\webimage.exe
 
 :: run installer
-%TEMP%\webimage_extracted\bootstrapper.exe  --action install --components=default --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=1 
+%TEMP%\webimage_extracted\bootstrapper.exe -s --action install --components=intel.oneapi.win.dpcpp-compiler:intel.oneapi.win.ifort-compiler:intel.oneapi.win.mkl.devel --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=1   --log-dir=%TEMP%
 
 :: download and unpack installer - HPC KIT
-curl.exe --output %TEMP%\webimage.exe --url %2 --retry 5 --retry-delay 5
-start /b /wait %TEMP%\webimage.exe  -x -f %TEMP%\webimage_extracted 
+curl.exe --output %TEMP%\webimage.exe --url %2 --retry 5 --retry-delay 5  --log %TEMP%\extract.log
+start /b /wait %TEMP%\webimage.exe -s -x -f %TEMP%\webimage_extracted 
 del %TEMP%\webimage.exe
 
 :: run installer 
-%TEMP%\webimage_extracted\bootstrapper.exe  --action install --components=default --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=1 
+%TEMP%\webimage_extracted\bootstrapper.exe -s --action install --intel.oneapi.win.ifort-compiler:intel.oneapi.win.mpi.devel:intel.oneapi.win.cpp-compiler --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=1  --log-dir=%TEMP%
