629b30cafc4a839b6b9c837ac21fa0bbedfd4159
Codi McKee <cmckee@tamu.edu>
2023-08-10 00:40:27 -0500

    Update install-intel-windows.bat

--- install-intel-windows.bat
+++ install-intel-windows.bat
@@ -1,9 +1,17 @@
 @echo off
 
-:: download and unpack installer
+:: download and unpack installer - BASEKIT
 curl.exe --output %TEMP%\webimage.exe --url %1 --retry 5 --retry-delay 5
 start /b /wait %TEMP%\webimage.exe -s -x -f %TEMP%\webimage_extracted --log %TEMP%\extract.log
 del %TEMP%\webimage.exe
 
 :: run installer
-%TEMP%\webimage_extracted\bootstrapper.exe -s --action install --components=intel.oneapi.win.cpp-compiler:intel.oneapi.win.ifort-compiler --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --log-dir=%TEMP%
+%TEMP%\webimage_extracted\bootstrapper.exe -s --action install --components=default --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=1 --log-dir=%TEMP%
+
+:: download and unpack installer - HPC KIT
+curl.exe --output %TEMP%\webimage.exe --url %2 --retry 5 --retry-delay 5
+start /b /wait %TEMP%\webimage.exe -s -x -f %TEMP%\webimage_extracted --log %TEMP%\extract.log
+del %TEMP%\webimage.exe
+
+:: run installer 
+%TEMP%\webimage_extracted\bootstrapper.exe -s --action install --components=default --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=1 --log-dir=%TEMP%
